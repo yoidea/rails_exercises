f1ac7971526d7d6f7636a7a8e6489e26fbdab2b4
yoidea <yoidea.cc@gmail.com>
2018-06-15 16:26:16 +0900

    Update files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
   def new
-  	@text = params[:text]
-  	Post.new(text: @text).save
+    @text = params[:text]
+    if @text != ''
+      Post.new(text: @text).save
+    end
+  end
+  def del
+    @id = params[:id]
+    if @id != ''
+      Post.find(text: @text).save
+    end
   end
   def index
-  	@posts = Post.all
+    @posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/new'
+  get 'posts/del'
   get 'posts/index'
   root to: 'home#top'  
 end
