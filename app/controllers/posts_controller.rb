396226f36a6dc5588d2e414923213b49c078a9e2
Yoidea <yoidea.cc@gmail.com>
2018-06-16 12:10:15 +0900

    Use params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,26 @@
 class PostsController < ApplicationController
+  # メソッドの中にはコメント書かない習慣らしい
   def new
     @text = params[:text]
     if @text != ''
       Post.new(text: @text).save
+      redirect_to :action => "index"
     end
   end
+
   def del
     @id = params[:id]
     if @id != ''
       Post.find(@id).delete
+      redirect_to :action => "index"
     end
   end
+
+  def show
+  	@id = params[:id]
+  	@post = Post.find(@id)
+  end
+
   def index
     @posts = Post.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'posts/new'
-  get 'posts/del'
+  get '/posts/del/:id', to: 'posts#del'
+  get '/posts/show/:id', to: 'posts#show'
   get 'posts/index'
   root to: 'home#top'  
 end
